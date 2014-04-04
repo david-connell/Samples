155d493784c5f1b7c45873fd922ccf0f159e52d4
David Connell <angus.connell@ntlworld.com>
2014-04-04 09:36:57 +0100

    Assembly info changes

--- CommunicationWithIdealFinish/InterProcessCommunication/InterProcessCommunication/Properties/AssemblyInfo.cs
+++ CommunicationWithIdealFinish/InterProcessCommunication/InterProcessCommunication/Properties/AssemblyInfo.cs
@@ -5,23 +5,18 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("InterProcessCommunication")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("TQC.GradientOven.InterProcessCommunication")]
+[assembly: AssemblyDescription("Communication between the gradient oven and Ideal Finish")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("InterProcessCommunication")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCompany("TQC")]
+[assembly: AssemblyProduct("TQC.GradientOven.InterProcessCommunication")]
+[assembly: AssemblyCopyright("Copyright © TQC 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("efcac9d4-ce09-4ae4-a20a-9d31f32b40e0")]
-
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
@@ -32,5 +27,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
