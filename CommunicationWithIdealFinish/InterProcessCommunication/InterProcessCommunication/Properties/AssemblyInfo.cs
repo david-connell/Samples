dcd136679a32d26fda4f14b472560e975ebc0b0d
David Connell <angus.connell@ntlworld.com>
2014-05-26 17:22:57 +0100

    *1.0.0.2

--- CommunicationWithIdealFinish/InterProcessCommunication/InterProcessCommunication/Properties/AssemblyInfo.cs
+++ CommunicationWithIdealFinish/InterProcessCommunication/InterProcessCommunication/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("TQC.GradientOven.InterProcessCommunication")]
-[assembly: AssemblyDescription("Communication between the gradient oven and Ideal Finish")]
+[assembly: AssemblyDescription("Communication between the Gradient Oven and TQC Ideal Finish Analysis")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("TQC")]
 [assembly: AssemblyProduct("TQC.GradientOven.InterProcessCommunication")]
@@ -27,5 +27,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.1")]
-[assembly: AssemblyFileVersion("1.0.0.1")]
+[assembly: AssemblyVersion("1.0.0.2")]
