e0bf27727efb35fbe5d7a5359d4a0deef1a5832e
David Connell <angus.connell@ntlworld.com>
2014-07-18 08:40:13 +0100

    *1.0.1.0 Try again as I can see previous attempt

--- LowLevelUSBComms/TQC.USBDevice/TQC.USBDevice/Properties/AssemblyInfoShared.cs
+++ LowLevelUSBComms/TQC.USBDevice/TQC.USBDevice/Properties/AssemblyInfoShared.cs
@@ -15,4 +15,4 @@ using System.Runtime.InteropServices;
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("1.0.0.2")]
+[assembly: AssemblyVersion("1.0.1.0")]
