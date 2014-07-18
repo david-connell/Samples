8974a3ad1aef83a273b9d0d832164b865b6579b0
David Connell <angus.connell@ntlworld.com>
2014-07-18 13:40:05 +0100

    Changed tests to reflect what they are actually doing

--- LowLevelUSBComms/TQC.USBDevice/IntegrationTest(nUnit)/GROMotherBoardCommands.cs
+++ LowLevelUSBComms/TQC.USBDevice/IntegrationTest(nUnit)/GROMotherBoardCommands.cs
@@ -20,46 +20,5 @@ namespace IntegrationTestNUnit
             ProductId = product;
         }
 
-        [TestCase(1)]
-        public void ReadProbes(byte thermocoupleBoardId)
-        {
-            using (var thermocoupleBoard = new ThermocoupleBoard(ProductId, thermocoupleBoardId))
-            {                    
-                var probeValues = thermocoupleBoard.Board.ProbeValues;
-                Assert.IsTrue(probeValues.Count() == 8);
-                foreach (var value in probeValues)
-                {
-                    Assert.IsTrue(value > 0);
-                }
-            }
-        }
-
-        [TestCase(1)]
-        public void ReadNumberOfProbes(byte thermocoupleBoardId)
-        {
-            using (var thermocoupleBoard = new ThermocoupleBoard(ProductId, thermocoupleBoardId))
-            {
-                var numberOfProbes = thermocoupleBoard.Board.NumberOfProbes;
-                Assert.That(numberOfProbes, Is.EqualTo(8));
-            }
-        }
-
-
-        [TestCase(1, 0, USBLogger.ProbeType.Temperature)]
-        [TestCase(1, 1, USBLogger.ProbeType.Temperature)]
-        [TestCase(1, 2, USBLogger.ProbeType.Temperature)]
-        [TestCase(1, 3, USBLogger.ProbeType.Temperature)]
-        [TestCase(1, 4, USBLogger.ProbeType.Temperature)]
-        [TestCase(1, 5, USBLogger.ProbeType.Temperature)]
-        [TestCase(1, 6, USBLogger.ProbeType.Temperature)]
-        [TestCase(1, 7, USBLogger.ProbeType.Temperature)]
-        public void ReadProbeType(byte thermocoupleBoardId, byte probeId, USBLogger.ProbeType probeType)
-        {
-            using (var thermocoupleBoard = new ThermocoupleBoard(ProductId, thermocoupleBoardId))
-            {
-                var type = thermocoupleBoard.Board.ProbeType(probeId);
-                Assert.That(type, Is.EqualTo(probeType));
-            }
-        }
     }
 }
---
+++ LowLevelUSBComms/TQC.USBDevice/IntegrationTest(nUnit)/GROThermocoupleBoardTests.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using TQC.USBDevice;
+
+namespace IntegrationTestNUnit
+{
+    [TestFixture(USBLogger.USBProductId.GRADIENT_OVEN, 1)]
+    class GROThermocoupleBoardTests
+    {
+        USBLogger.USBProductId ProductId { get; set; }
+        byte ThermocoupleBoard { get; set; }
+
+        public GROThermocoupleBoardTests(USBLogger.USBProductId product, byte thermocoupleBoard)
+        {
+            ProductId = product;
+            ThermocoupleBoard = thermocoupleBoard;
+        }
+
+        
+        public void ReadProbes()
+        {
+            using (var thermocoupleBoard = new ThermocoupleBoard(ProductId, ThermocoupleBoard))
+            {
+                var probeValues = thermocoupleBoard.Board.ProbeValues;
+                Assert.IsTrue(probeValues.Count() == 8);
+                foreach (var value in probeValues)
+                {
+                    Assert.IsTrue(value > 0);
+                }
+            }
+        }
+
+        
+        public void ReadNumberOfProbes()
+        {
+            using (var thermocoupleBoard = new ThermocoupleBoard(ProductId, ThermocoupleBoard))
+            {
+                var numberOfProbes = thermocoupleBoard.Board.NumberOfProbes;
+                Assert.That(numberOfProbes, Is.EqualTo(8));
+            }
+        }
+
+
+        [TestCase(0, USBLogger.ProbeType.Temperature)]
+        [TestCase(1, USBLogger.ProbeType.Temperature)]
+        [TestCase(2, USBLogger.ProbeType.Temperature)]
+        [TestCase(3, USBLogger.ProbeType.Temperature)]
+        [TestCase(4, USBLogger.ProbeType.Temperature)]
+        [TestCase(5, USBLogger.ProbeType.Temperature)]
+        [TestCase(6, USBLogger.ProbeType.Temperature)]
+        [TestCase(7, USBLogger.ProbeType.Temperature)]
+        public void ReadProbeType(byte probeId, USBLogger.ProbeType probeType)
+        {
+            using (var thermocoupleBoard = new ThermocoupleBoard(ProductId, ThermocoupleBoard))
+            {
+                var type = thermocoupleBoard.Board.ProbeType(probeId);
+                Assert.That(type, Is.EqualTo(probeType));
+            }
+        }
+
+    }
+}
