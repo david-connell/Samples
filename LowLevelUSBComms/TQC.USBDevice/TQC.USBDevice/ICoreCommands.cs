d9a77d364379c84a7ce465ba5e3c58bd72fbaeaf
david-connell <david.connell@red-gate.com>
2014-11-04 15:37:45 +0000

    Split out files

---
+++ LowLevelUSBComms/TQC.USBDevice/TQC.USBDevice/ICoreCommands.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TQC.USBDevice
+{
+    public interface ICoreCommands
+    {
+        DateTime ManufactureDate { get; }
+        string ManufactureName { get; }
+        Version ProtocolVersion { get; }
+        Version HardwareVersion { get; }
+        Version SoftwareVersion { get; }
+
+        string DeviceName { get; }
+        TQC.USBDevice.USBLogger.DeviceType DeviceType { get; }
+        int SerialNumber { get; }
+    }
+}
---
+++ LowLevelUSBComms/TQC.USBDevice/TQC.USBDevice/LinearCalibrationDetails.cs
@@ -0,0 +1,17 @@
+﻿namespace TQC.USBDevice
+{
+    public class LinearCalibrationDetails
+    {
+        public double M { get; private set; }
+        public double C { get; private set; }
+        public LinearCalibrationDetails(double m, double c)
+        {
+            M = m;
+            C = c;
+        }
+        public override string ToString()
+        {
+            return string.Format("Y={0}X + {1}", M, C);
+        }
+    }
+}
