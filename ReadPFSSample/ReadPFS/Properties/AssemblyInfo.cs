9d7d1ff1cea74597a8e43bc75324cd6e2ca0df31
David Connell <david.connell@red-gate.com>
2015-05-08 19:02:30 +0100

    *1.0.1 for PFSWrapper

--- ReadPFSSample/ReadPFS/Properties/AssemblyInfo.cs
+++ ReadPFSSample/ReadPFS/Properties/AssemblyInfo.cs
@@ -5,14 +5,14 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PFSWrapper")]
+[assembly: AssemblyTitle("TQC.IFA.PFSWrapper")]
 [assembly: AssemblyDescription(".NET Wrapper for reading PFS Files")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("TQC")]
 [assembly: AssemblyProduct("ReadPFS")]
-[assembly: AssemblyCopyright("Copyright TQC © 2014")]
+[assembly: AssemblyCopyright("Copyright TQC © 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("1.0.0.1")]
-[assembly: AssemblyFileVersion("1.0.0.1")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
