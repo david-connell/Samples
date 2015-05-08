fe7efd0c365f3423788890682289530ebc3f49ac
David Connell <david.connell@red-gate.com>
2015-05-08 18:32:36 +0100

    Minor change to assembly name

--- LowLevelUSBComms/TQC.USBDevice/TQC.USBDevice/Properties/AssemblyInfo.cs
+++ LowLevelUSBComms/TQC.USBDevice/TQC.USBDevice/Properties/AssemblyInfo.cs
@@ -5,11 +5,9 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TQC.USBDevice")]
+[assembly: AssemblyTitle("TQC.USB.Device")]
 [assembly: AssemblyDescription(".NET Wrapper around TQC USB devices")]
-[assembly: AssemblyProduct("TQC.USBDevice")]
-
-
+[assembly: AssemblyProduct("TQC.USB.Device")]
 
 [assembly: InternalsVisibleTo("IntegrationTestProject1")]
 [assembly: InternalsVisibleTo("IntegrationTest(nUnit)")]
