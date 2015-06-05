e3d16ce4b9b803917b57f036d0e92e7a1c4f1847
David Connell <david.connell@red-gate.com>
2015-06-05 22:47:14 +0100

    Fixed using port name :(

--- LowLevelUSBComms/TQC.USBDevice/TQC.USBDevice/GlossMeter/TmrLogger.cs
+++ LowLevelUSBComms/TQC.USBDevice/TQC.USBDevice/GlossMeter/TmrLogger.cs
@@ -1,29 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace TQC.USBDevice.GlossMeter
+﻿namespace TQC.USBDevice.GlossMeter
 {
-
     public class TmrLogger : GlossMeterLogger
     {
         public bool Open(bool minimumCommunications = false)
-        {
-            string portName = null;
+        {            
             var code = new UsbDevices();
             foreach (var item in code.GetUSBDevices())
             {
                 if (item.FriendlyName.ToLower().StartsWith("trm "))
                 {
-                    return Open(USBProductId.Glossmeter, portName, minimumCommunications);
+                    return Open(USBProductId.Glossmeter, item.DeviceName, minimumCommunications);
                 }
             }
             return false;
         }
-
-
-
-
     }
 }
