84d6d91407590d7cf9c4e96cf2d9237c80987c20
David Connell <david.connell@red-gate.com>
2015-09-04 10:01:05 +0100

    *V1.3.13
    Better tests over TRM

--- LowLevelUSBComms/TQC.USBDevice/IntegrationTest(nUnit)/Logger/Glossmeter/Basic.cs
+++ LowLevelUSBComms/TQC.USBDevice/IntegrationTest(nUnit)/Logger/Glossmeter/Basic.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using IntegrationTestNUnit.Logger.GeneralLogger;
 using NUnit.Framework;
 using TQC.USBDevice;
+using TQC.USBDevice.GlossMeter;
 
 namespace IntegrationTestNUnit.Logger.GlossMeter
 {
@@ -16,6 +17,214 @@ namespace IntegrationTestNUnit.Logger.GlossMeter
             : base(USBLogger.USBProductId.Glossmeter)
         {
         }
+
+        protected virtual GlossMeterLogger OpenGlossMeterLogger(bool miniumum = true)
+        {
+            var logger = new GlossMeterLogger();
+            if (logger.Open(USBLogger.USBProductId.Glossmeter, miniumum))
+            {
+                return logger;
+            }
+            throw new Exception("Failed to connect to logger " + ProductId.ToString());
+
+        }
+
+
+
+        [Test]
+        public void CheckSerialNumber()
+        {
+            using (var logger = OpenLogger(false))
+            {
+                Console.WriteLine("Logger serial Number is: '{0}'", logger.LoggerSerialNumber);
+                Console.WriteLine("Version: '{0}'", logger.Version);
+
+
+                logger.Close();
+            }
+        }
+        [Test]
+        public void CheckBuzz()
+        {
+            using (var logger = OpenGlossMeterLogger(false))
+            {
+
+                logger.Buzzer(1, 10, 2, 10);
+                logger.Close();
+            }
+        }
+        [TestCase(0, "Line 0")]
+        [TestCase(1, "Line 1")]
+        [TestCase(2, "Line 2")]
+        [TestCase(3, "Line 3")]
+        [TestCase(4, "Line 4")]
+        [TestCase(5, "Line 5")]
+        public void CheckText(int lineNo, string text)
+        {
+            using (var logger = OpenGlossMeterLogger(false))
+            {
+
+                logger.WriteTextString(lineNo, text);
+                logger.Close();
+            }
+        }
+
+        [Test]
+        public void CheckResetScreen()
+        {
+            using (var logger = OpenGlossMeterLogger(false))
+            {
+
+                logger.ResetScreen();
+                logger.Close();
+            }
+        }
+
+
+        [TestCase(0)]
+        public void CheckCoefficients(int channelId)
+        {
+            using (var logger = OpenGlossMeterLogger(false))
+            {
+
+                var coeffs = logger.get_CalibrationCoeffients(channelId);
+                //Assert.That(coeffs.Count, Is.EqualTo(5));
+                foreach (var coeff in coeffs)
+                {
+                    Console.WriteLine(coeff);
+                }
+                logger.Close();
+            }
+        }
+
+        [TestCase(0)]
+        public void CheckSettingCoefficients(int channelId)
+        {
+            using (var logger = OpenGlossMeterLogger(false))
+            {
+
+                var coeffs = logger.get_CalibrationCoeffients(channelId);
+                foreach (var coeff in coeffs)
+                {
+                    Console.WriteLine("Read {0}", coeff);
+                }
+
+                //Assert.That(coeffs.Count, Is.EqualTo(5));
+                coeffs[1] += 0.1;
+                foreach (var coeff in coeffs)
+                {
+                    Console.WriteLine("Write {0}", coeff);
+                }
+
+                logger.set_CalibrationCoeffients(channelId, coeffs);
+                coeffs = logger.get_CalibrationCoeffients(channelId);
+                foreach (var coeff in coeffs)
+                {
+                    Console.WriteLine("Read {0}", coeff);
+                }
+                logger.Close();
+            }
+        }
+
+        [Test]
+        public void CheckInternalGloss()
+        {
+            using (var logger = OpenGlossMeterLogger(false))
+            {
+
+                var result = logger.InternalGloss;
+
+                Assert.That(result.Count, Is.EqualTo(3));
+                foreach (var value in result)
+                {
+                    Console.WriteLine(value);
+                    Assert.That(value, Is.GreaterThan(50.0).And.LessThan(150));
+                }
+
+
+                logger.Close();
+            }
+        }
+
+        [Test]
+        public void CheckSettingOfInternalGloss()
+        {
+            using (var logger = OpenGlossMeterLogger(false))
+            {
+
+                var result = logger.InternalGloss;
+
+
+                var vals = new List<double>();
+                vals.Add(100);
+                vals.Add(200);
+                vals.Add(300);
+                logger.InternalGloss = vals;
+
+                var temp = logger.InternalGloss;
+                for (int id = 0; id < 3; id++)
+                {
+                    Assert.That(temp[id], Is.EqualTo((id + 1) * 100));
+                }
+                logger.InternalGloss = result;
+
+
+                logger.Close();
+            }
+        }
+
+        [Test]
+        public void TestScan()
+        {
+            TestButton(x => x.IsScanButtonPressed, "SCAN");
+        }
+
+        [Test]
+        public void TestOk()
+        {
+            TestButton(x => x.IsOKPressed, "OK");
+        }
+
+
+        [Test]
+        public void TestUp()
+        {
+            TestButton(x => x.IsUpPressed, "Up");
+        }
+
+        [Test]
+        public void TestDown()
+        {
+            TestButton(x => x.IsDownPressed, "Down");
+        }
+
+        [Test]
+        public void TestInCraddle()
+        {
+            TestButton(x => x.IsInCraddle, "Craddle");
+        }
+
+        private void TestButton(Func<GlossMeterLogger, bool> buttonToPress, string nameOfButton)
+        {
+            using (var logger = OpenGlossMeterLogger(false))
+            {
+                DateTime until = DateTime.Now.AddSeconds(10);
+                while (DateTime.Now < until)
+                {
+                    Assert.That(logger.ReadButtonStatus(), Is.EqualTo(true));
+                    Console.WriteLine("Looking for {0}", nameOfButton);
+                    if (buttonToPress(logger))
+                    {
+                        break;
+                    }
+                }
+                Assert.That(buttonToPress(logger), Is.EqualTo(true));
+
+                logger.Close();
+            }
+        }
+
+
     }
 
     [TestFixture]
--- LowLevelUSBComms/TQC.USBDevice/IntegrationTest(nUnit)/Logger/Glossmeter/TRMGlossMeter.cs
+++ LowLevelUSBComms/TQC.USBDevice/IntegrationTest(nUnit)/Logger/Glossmeter/TRMGlossMeter.cs
@@ -5,20 +5,20 @@ using NUnit.Framework;
 using TQC.USBDevice;
 using TQC.USBDevice.GlossMeter;
 
-namespace IntegrationTestNUnit.Logger.Glossmeter
+namespace IntegrationTestNUnit.Logger.GlossMeter
 {
     [TestFixture]
-    class TrmGlossMeterBasicCommands : BasicCommands
+    class TrmGlossMeterBasicCommands : Basic
     {
-        public TrmGlossMeterBasicCommands() : base(USBLogger.USBProductId.Glossmeter)           
+        public TrmGlossMeterBasicCommands() //: base(USBLogger.USBProductId.Glossmeter)
         {
         }
         protected override TQCUsbLogger OpenLogger(bool miniumum = true)
         {
-            return OpenTrmLogger(miniumum);
+            return OpenGlossMeterLogger(miniumum);
         }
 
-        TrmLogger OpenTrmLogger(bool miniumum = true)
+        override protected GlossMeterLogger OpenGlossMeterLogger(bool miniumum = true)
         {
             var logger = new TrmLogger();
             if (logger.Open(miniumum))
@@ -30,22 +30,9 @@ namespace IntegrationTestNUnit.Logger.Glossmeter
         }
 
         [Test]
-        public void CheckSerialNumber()
-        {
-            using (var logger = OpenLogger(false))
-            {
-                Console.WriteLine("Logger serial Number is: '{0}'", logger.LoggerSerialNumber);
-                Console.WriteLine("Version: '{0}'", logger.Version);
-                
-                
-                logger.Close();
-            }
-        }
-
-        [Test]
         public void CheckTrm()
         {
-            using (var logger = OpenTrmLogger(false))
+            using (var logger = OpenGlossMeterLogger(false))
             {
 
                 Assert.That(logger.IsTmr, Is.EqualTo(true), "TRM is not set!");
@@ -55,186 +42,6 @@ namespace IntegrationTestNUnit.Logger.Glossmeter
         }
 
 
-        [Test]
-        public void CheckBuzz()
-        {
-            using (var logger = OpenTrmLogger(false))
-            {
-
-                logger.Buzzer(1, 10, 2, 10);
-                logger.Close();
-            }
-        }
-        [TestCase(0, "Line 0")]
-        [TestCase(1, "Line 1")]
-        [TestCase(2, "Line 2")]
-        [TestCase(3, "Line 3")]
-        [TestCase(4, "Line 4")]
-        [TestCase(5, "Line 5")]
-        public void CheckText(int lineNo, string text)
-        {
-            using (var logger = OpenTrmLogger(false))
-            {
-
-                logger.WriteTextString(lineNo, text);
-                logger.Close();
-            }
-        }
-
-        [Test]
-        public void CheckResetScreen()
-        {
-            using (var logger = OpenTrmLogger(false))
-            {
-
-                logger.ResetScreen();
-                logger.Close();
-            }
-        }
-
-
-        [TestCase(0)]
-        public void CheckCoefficients(int channelId)
-        {
-            using (var logger = OpenTrmLogger(false))
-            {
-
-                var coeffs = logger.get_CalibrationCoeffients(channelId);
-                //Assert.That(coeffs.Count, Is.EqualTo(5));
-                foreach (var coeff in coeffs)
-                {
-                    Console.WriteLine(coeff);
-                }
-                logger.Close();
-            }
-        }
-
-        [TestCase(0)]
-        public void CheckSettingCoefficients(int channelId)
-        {
-            using (var logger = OpenTrmLogger(false))
-            {
-
-                var coeffs = logger.get_CalibrationCoeffients(channelId);
-                foreach (var coeff in coeffs)
-                {
-                    Console.WriteLine("Read {0}", coeff);
-                }
-
-                //Assert.That(coeffs.Count, Is.EqualTo(5));
-                coeffs[1] += 0.1;
-                foreach (var coeff in coeffs)
-                {
-                    Console.WriteLine("Write {0}", coeff);
-                }
-
-                logger.set_CalibrationCoeffients(channelId, coeffs);
-                coeffs = logger.get_CalibrationCoeffients(channelId);
-                foreach (var coeff in coeffs)
-                {
-                    Console.WriteLine("Read {0}", coeff);
-                }
-                logger.Close();
-            }
-        }
-
-        [Test]
-        public void CheckInternalGloss()
-        {
-            using (var logger = OpenTrmLogger(false))
-            {
-
-                var result = logger.InternalGloss;
-
-                Assert.That(result.Count, Is.EqualTo(3));
-                foreach (var value in result)
-                {
-                    Console.WriteLine(value);
-                    Assert.That(value, Is.GreaterThan(50.0).And.LessThan(150));
-                }
-                
-                
-                logger.Close();
-            }
-        }
-
-        [Test]
-        public void CheckSettingOfInternalGloss()
-        {
-            using (var logger = OpenTrmLogger(false))
-            {
-
-                var result = logger.InternalGloss;
-
-
-                var vals = new List<double>();
-                vals.Add(100);
-                vals.Add(200);
-                vals.Add(300);
-                logger.InternalGloss = vals;
-
-                var temp = logger.InternalGloss;
-                for (int id = 0; id < 3; id++)
-                {
-                    Assert.That(temp[id], Is.EqualTo((id+1) * 100));
-                }
-                logger.InternalGloss = result;
-
-
-                logger.Close();
-            }
-        }
-
-        [Test]
-        public void TestScan()
-        {
-            TestButton(x => x.IsScanButtonPressed, "SCAN");
-        }
-
-        [Test]
-        public void TestOk()
-        {
-            TestButton(x => x.IsOKPressed, "OK");
-        }
-
-
-        [Test]
-        public void TestUp()
-        {
-            TestButton(x => x.IsUpPressed, "Up");
-        }
-
-        [Test]
-        public void TestDown()
-        {
-            TestButton(x => x.IsDownPressed, "Down");
-        }
-
-        [Test]
-        public void TestInCraddle()
-        {
-            TestButton(x => x.IsInCraddle, "Craddle");
-        }
-
-        public void TestButton(Func<TrmLogger, bool> buttonToPress, string nameOfButton)
-        {
-            using (var logger = OpenTrmLogger(false))
-            {
-                DateTime until = DateTime.Now.AddSeconds(10);
-                while (DateTime.Now < until)
-                {                    
-                    Assert.That(logger.ReadButtonStatus(), Is.EqualTo(true));
-                    Console.WriteLine("Looking for {0}", nameOfButton);                    
-                    if (buttonToPress(logger))
-                    {
-                        break;
-                    }
-                }
-                Assert.That(buttonToPress(logger), Is.EqualTo(true));
-
-                logger.Close();
-            }
-        }
 
 
 
