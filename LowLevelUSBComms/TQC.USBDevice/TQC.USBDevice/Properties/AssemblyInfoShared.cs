b50b551b7ffc9145cdfecc7fc0fdf45cc20f492e
David Connell <david.connell@red-gate.com>
2015-10-16 11:52:24 +0100

    *V1.4.8 USBComms

--- LowLevelUSBComms/TQC.USBDevice/TQC.USBDevice/Properties/AssemblyInfoShared.cs
+++ LowLevelUSBComms/TQC.USBDevice/TQC.USBDevice/Properties/AssemblyInfoShared.cs
@@ -12,5 +12,5 @@ using System.Runtime.InteropServices;
 //
 //Change build.bat
 //
-[assembly: AssemblyVersion("1.4.7.*")]
-//[assembly: AssemblyVersion("1.3.11.24747")]
+[assembly: AssemblyVersion("1.4.8.*")]
+
